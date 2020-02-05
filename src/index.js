efea5bfcb5a5c8670b22c6d7c7f198b928fdba10
Jake Thomas <jmaxt12@gmail.com>
2020-02-05 11:14:49 -0700

    added interceptors to index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,19 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+    return request
+}, error => {
+    Promise.reject(error)
+})
+
+axios.interceptors.request.use(response => {
+    return response;
+}, error => {
+    Promise.reject(error)
+})
+
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
